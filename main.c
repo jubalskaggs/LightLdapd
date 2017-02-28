bf127a2349cb6d241b5e6b611028031ba0f835bb
Donovan Baarda <abo@minkirri.apana.org.au>
2017-02-28 22:37:40 +1100

    Implement proper partial recv/send support.
    Add ldap_connection_send() and ldap_connection_recv() functions that
    encode/decode messages to the send/recv buffers returning RC_MORE if
    the action was incomplete.
    
    Add ldap_request_init(), ldap_request_done() for
    intializing/destroying a request's state. Add ldap_request_reply()
    methods for replying to requests, returning RC_MORE if the reply is
    incomplete and blocked by a full buffer or delay.
    
    Add ldap_connection_respond() method that repeatedly recvs and replies
    from/to the buffers until blocked by empty/full buffers and/or
    partialy completed decoding/replying.
    
    Make the read_cb() and write_cb() just read/write from/to the buffers
    and call ldap_connection_respond() to process them. Make delay_cb()
    also just call ldap_connection_respond() to send the delayed response.
    
    Make ldap_request_bind() and ldap_request_search() return RC_MORE if
    they get blocked. Make them capable or resuming when called again
    after being blocked.

--- main.c
+++ main.c
@@ -63,6 +63,7 @@ void buffer_consumed(buffer_t *buffer, size_t len);
 #define buffer_rpos(buffer) ((buffer)->buf)
 #define buffer_rlen(buffer) ((buffer)->len)
 #define buffer_empty(buffer) (!(buffer)->len)
+#define buffer_full(buffer) ((buffer)->len == BUFFER_SIZE)
 
 typedef struct {
 	char *basedn;
@@ -75,27 +76,38 @@ int ldap_server_start(ldap_server *server, uint32_t addr, int port);
 void ldap_server_stop(ldap_server *server);
 char *ldap_server_cn2name(ldap_server *server, const char *cn);
 
+/* Reuse the ber_decode return value enum as the ldap recv/send status. */
+typedef enum asn_dec_rval_code_e ldap_status_t;
+
 typedef struct {
 	ldap_server *server;
 	ev_io read_watcher;
 	ev_io write_watcher;
 	ev_timer delay_watcher;
 	LDAPMessage_t *request;
+	ldap_status_t request_status;
 	LDAPMessage_t *response;
+	ldap_status_t response_status;
+	int response_stage;
 	buffer_t recv_buf;
 	buffer_t send_buf;
 } ldap_connection;
 ldap_connection *ldap_connection_new(ldap_server *server, int fd);
 void ldap_connection_free(ldap_connection *connection);
-void ldap_connection_send(ldap_connection *connection, LDAPMessage_t *msg);
+void ldap_connection_respond(ldap_connection *connection);
+ldap_status_t ldap_connection_send(ldap_connection *connection, LDAPMessage_t *msg);
+ldap_status_t ldap_connection_recv(ldap_connection *connection, LDAPMessage_t **msg);
 
 void accept_cb(ev_loop *loop, ev_io *watcher, int revents);
 void read_cb(ev_loop *loop, ev_io *watcher, int revents);
 void write_cb(ev_loop *loop, ev_io *watcher, int revents);
 void delay_cb(EV_P_ ev_timer *w, int revents);
 
-void ldap_request_bind(ldap_connection *connection, int msgid, BindRequest_t *req);
-void ldap_request_search(ldap_connection *connection, int msgid, SearchRequest_t *req);
+void ldap_request_init(ldap_connection *connection);
+void ldap_request_done(ldap_connection *connection);
+ldap_status_t ldap_request_reply(ldap_connection *connection, LDAPMessage_t *req);
+ldap_status_t ldap_request_bind(ldap_connection *connection, int msgid, BindRequest_t *req);
+ldap_status_t ldap_request_search(ldap_connection *connection, int msgid, SearchRequest_t *req);
 
 typedef struct {
 	const char *user, *pw;
@@ -217,30 +229,80 @@ ldap_connection *ldap_connection_new(ldap_server *server, int fd)
 	connection->delay_watcher.data = connection;
 	buffer_init(&connection->recv_buf);
 	buffer_init(&connection->send_buf);
+	ldap_request_init(connection);
 	ev_io_start(server->loop, &connection->read_watcher);
 	return connection;
 }
 
 void ldap_connection_free(ldap_connection *connection)
 {
-	assert(ev_is_active(&connection->read_watcher));
-
 	ev_io_stop(connection->server->loop, &connection->read_watcher);
 	ev_io_stop(connection->server->loop, &connection->write_watcher);
 	ev_timer_stop(connection->server->loop, &connection->delay_watcher);
 	close(connection->read_watcher.fd);
+	ldap_request_done(connection);
 	free(connection);
 }
 
-void ldap_connection_send(ldap_connection *connection, LDAPMessage_t *msg)
+void ldap_connection_respond(ldap_connection *connection)
+{
+	ldap_server *server = connection->server;
+	LDAPMessage_t **req = &connection->request;
+
+	/* Recieve and reply to requests until blocked on recv or reply. */
+	do {
+		if (connection->response_status == RC_OK) {
+			ldap_request_done(connection);
+			ldap_request_init(connection);
+		}
+		if (connection->request_status == RC_WMORE)
+			connection->request_status = ldap_connection_recv(connection, req);
+		if (connection->request_status == RC_OK)
+			connection->response_status = ldap_request_reply(connection, *req);
+	} while (connection->response_status == RC_OK);
+	if (connection->request_status == RC_FAIL) {
+		ldap_connection_free(connection);
+		return;
+	}
+	if (buffer_full(&connection->recv_buf)) {
+		ev_io_stop(server->loop, &connection->read_watcher);
+	} else {
+		ev_io_start(server->loop, &connection->read_watcher);
+	}
+	if (buffer_empty(&connection->send_buf))
+		ev_io_stop(server->loop, &connection->write_watcher);
+	else
+		ev_io_start(server->loop, &connection->write_watcher);
+}
+
+ldap_status_t ldap_connection_send(ldap_connection *connection, LDAPMessage_t *msg)
 {
 	buffer_t *buf = &connection->send_buf;
 	asn_enc_rval_t rencode;
 
-	LDAP_DEBUG(msg);
 	rencode = der_encode_to_buffer(&asn_DEF_LDAPMessage, msg, buffer_wpos(buf), buffer_wlen(buf));
+	/* If it failed, the buffer was probably full, return RC_WMORE to say try again next time. */
+	if (rencode.encoded == -1)
+		return RC_WMORE;
 	buffer_appended(buf, rencode.encoded);
-	ev_io_start(connection->server->loop, &connection->write_watcher);
+	LDAP_DEBUG(msg);
+	return RC_OK;
+}
+
+ldap_status_t ldap_connection_recv(ldap_connection *connection, LDAPMessage_t **msg)
+{
+	buffer_t *buf = &connection->recv_buf;
+	asn_dec_rval_t rdecode;
+
+	/* from asn1c's FAQ: If you want data to be BER or DER encoded, just invoke der_encode(). */
+	rdecode = ber_decode(0, &asn_DEF_LDAPMessage, (void **)msg, buffer_rpos(buf), buffer_rlen(buf));
+	buffer_consumed(buf, rdecode.consumed);
+	if (rdecode.code == RC_FAIL) {
+		fail1("ber_decode", RC_FAIL);
+	} else if (rdecode.code == RC_OK) {
+		LDAP_DEBUG(*msg);
+	}
+	return rdecode.code;
 }
 
 void accept_cb(ev_loop *loop, ev_io *watcher, int revents)
@@ -263,8 +325,6 @@ void read_cb(ev_loop *loop, ev_io *watcher, int revents)
 	ldap_connection *connection = watcher->data;
 	buffer_t *buf = &connection->recv_buf;
 	ssize_t buf_cnt;
-	LDAPMessage_t *req = NULL;
-	asn_dec_rval_t rdecode;
 
 	assert(connection->server->loop == loop);
 	assert(&connection->read_watcher == watcher);
@@ -279,30 +339,7 @@ void read_cb(ev_loop *loop, ev_io *watcher, int revents)
 		return;
 	}
 	buffer_appended(buf, buf_cnt);
-	/* from asn1c's FAQ: If you want data to be BER or DER encoded, just invoke der_encode(). */
-	rdecode = ber_decode(0, &asn_DEF_LDAPMessage, (void **)&req, buffer_rpos(buf), buffer_rlen(buf));
-	buffer_consumed(buf, rdecode.consumed);
-	if (rdecode.code != RC_OK || (ssize_t) rdecode.consumed != buf_cnt) {
-		ldap_connection_free(connection);
-		ldapmessage_free(req);
-		fail((rdecode.code != RC_OK) ? "der_decoder" : "consumed");
-	}
-	LDAP_DEBUG(req);
-	switch (req->protocolOp.present) {
-	case LDAPMessage__protocolOp_PR_bindRequest:
-		ldap_request_bind(connection, req->messageID, &req->protocolOp.choice.bindRequest);
-		break;
-	case LDAPMessage__protocolOp_PR_searchRequest:
-		ldap_request_search(connection, req->messageID, &req->protocolOp.choice.searchRequest);
-		break;
-	case LDAPMessage__protocolOp_PR_unbindRequest:
-		ldap_connection_free(connection);
-		break;
-	default:
-		perror("_|_");
-		ldap_connection_free(connection);
-	}
-	ldapmessage_free(req);
+	ldap_connection_respond(connection);
 }
 
 void write_cb(ev_loop *loop, ev_io *watcher, int revents)
@@ -320,35 +357,67 @@ void write_cb(ev_loop *loop, ev_io *watcher, int revents)
 		fail("send");
 	}
 	buffer_consumed(buf, buf_cnt);
-	if (buffer_empty(buf))
-		ev_io_stop(loop, watcher);
+	ldap_connection_respond(connection);
 }
 
 void delay_cb(ev_loop *loop, ev_timer *watcher, int revents)
 {
 	ldap_connection *connection = watcher->data;
-	LDAPMessage_t *res = connection->response;
 
 	assert(connection->server->loop == loop);
 	assert(&connection->delay_watcher == watcher);
 
-	ldap_connection_send(connection, res);
-	ldapmessage_free(res);
-	/* Restart the connection read_watcher after the delay. */
-	ev_io_start(loop, &connection->read_watcher);
+	ldap_connection_respond(connection);
 }
 
-void ldap_request_bind(ldap_connection *connection, int msgid, BindRequest_t *req)
+void ldap_request_init(ldap_connection *connection)
+{
+	connection->request = NULL;
+	connection->request_status = RC_WMORE;
+	connection->response = NULL;
+	connection->response_status = RC_WMORE;
+	connection->response_stage = 0;
+}
+
+void ldap_request_done(ldap_connection *connection)
+{
+	ldapmessage_free(connection->request);
+	ldapmessage_free(connection->response);
+}
+
+ldap_status_t ldap_request_reply(ldap_connection *connection, LDAPMessage_t *req)
+{
+	switch (req->protocolOp.present) {
+	case LDAPMessage__protocolOp_PR_bindRequest:
+		return ldap_request_bind(connection, req->messageID, &req->protocolOp.choice.bindRequest);
+	case LDAPMessage__protocolOp_PR_searchRequest:
+		return ldap_request_search(connection, req->messageID, &req->protocolOp.choice.searchRequest);
+	case LDAPMessage__protocolOp_PR_unbindRequest:
+		return RC_FAIL;
+	default:
+		perror("_|_");
+		return RC_FAIL;
+	}
+}
+
+ldap_status_t ldap_request_bind(ldap_connection *connection, int msgid, BindRequest_t *req)
 {
 	ldap_server *server = connection->server;
+	LDAPMessage_t *res = connection->response;
 	ev_tstamp delay = 0.0;
-	LDAPMessage_t *res = XNEW0(LDAPMessage_t, 1);
 
+	/* If the delay is active, do nothing and return RC_WMORE to say try again. */
+	if (ev_is_active(&connection->delay_watcher))
+		return RC_WMORE;
+	/* If we have already built the response, just try to send it. */
+	if (res)
+		return ldap_connection_send(connection, res);
+	/* Otherwise construct the response first. */
+	res = connection->response = XNEW0(LDAPMessage_t, 1);
 	res->messageID = msgid;
 	res->protocolOp.present = LDAPMessage__protocolOp_PR_bindResponse;
 	BindResponse_t *bindResponse = &res->protocolOp.choice.bindResponse;
 	OCTET_STRING_fromBuf(&bindResponse->matchedDN, (const char *)req->name.buf, req->name.size);
-
 	if (server->anonymous && req->name.size == 0) {
 		/* allow anonymous */
 		asn_long2INTEGER(&bindResponse->resultCode, BindResponse__resultCode_success);
@@ -371,59 +440,70 @@ void ldap_request_bind(ldap_connection *connection, int msgid, BindRequest_t *re
 		/* sasl or anonymous auth */
 		asn_long2INTEGER(&bindResponse->resultCode, BindResponse__resultCode_authMethodNotSupported);
 	}
+	/* If delay was set, pause response by starting delay watcher. */
 	if (delay > 0.0) {
-		connection->response = res;
 		ev_timer_set(&connection->delay_watcher, delay, 0.0);
-		/* Stop the connection read_watcher to stop other requests while delayed. */
-		ev_io_stop(server->loop, &connection->read_watcher);
 		ev_timer_start(server->loop, &connection->delay_watcher);
-	} else {
-		ldap_connection_send(connection, res);
-		ldapmessage_free(res);
+		return RC_WMORE;
 	}
+	return ldap_connection_send(connection, res);
 }
 
-void ldap_request_search(ldap_connection *connection, int msgid, SearchRequest_t *req)
+ldap_status_t ldap_request_search(ldap_connection *connection, int msgid, SearchRequest_t *req)
 {
 	ldap_server *server = connection->server;
+	LDAPMessage_t *res = connection->response;
+	ldap_status_t status = RC_WMORE;
 	/* (user=$username$) => cn=$username$,BASEDN */
 	char user[256];
-	LDAPMessage_t *res = XNEW0(LDAPMessage_t, 1);
-
+	/* Check that it's a valid search request. */
 	AttributeValueAssertion_t *attr = &req->filter.choice.equalityMatch;
 	int bad_dn = strcmp((const char *)req->baseObject.buf, server->basedn)
 	    && strcmp((const char *)req->baseObject.buf, "");
 	int bad_filter = req->filter.present != Filter_PR_equalityMatch
 	    || strcmp((const char *)attr->attributeDesc.buf, "user");
 
-	res->messageID = msgid;
-
-	if (!bad_dn && !bad_filter) {
-		/* result of search */
-		res->protocolOp.present = LDAPMessage__protocolOp_PR_searchResEntry;
-		SearchResultEntry_t *searchResEntry = &res->protocolOp.choice.searchResEntry;
-		snprintf(user, sizeof(user), "cn=%s,%s", (const char *)attr->assertionValue.buf, server->basedn);
-		OCTET_STRING_fromString(&searchResEntry->objectName, user);
-
-		ldap_connection_send(connection, res);
-		ldapmessage_empty(res);
+	if (connection->response_stage == 0) {
+		/* Allocate the response. */
+		res = connection->response = XNEW0(LDAPMessage_t, 1);
+		res->messageID = msgid;
+		status = RC_OK;
 	}
-
-	/* search is done */
-	res->protocolOp.present = LDAPMessage__protocolOp_PR_searchResDone;
-	SearchResultDone_t *searchResDone = &res->protocolOp.choice.searchResDone;
-	if (bad_dn) {
-		asn_long2INTEGER(&searchResDone->resultCode, LDAPResult__resultCode_other);
-		OCTET_STRING_fromString(&searchResDone->diagnosticMessage, "baseobject is invalid");
-	} else if (bad_filter) {
-		asn_long2INTEGER(&searchResDone->resultCode, LDAPResult__resultCode_other);
-		OCTET_STRING_fromString(&searchResDone->diagnosticMessage, "filter not supported");
-	} else {
-		asn_long2INTEGER(&searchResDone->resultCode, LDAPResult__resultCode_success);
-		OCTET_STRING_fromString(&searchResDone->matchedDN, server->basedn);
-	}
-	ldap_connection_send(connection, res);
-	ldapmessage_free(res);
+	do {
+		/* If we need to, create a new response. */
+		if (status == RC_OK) {
+			/* Empty the response message. */
+			ldapmessage_empty(res);
+			if (connection->response_stage == 0 && !bad_dn && !bad_filter) {
+				/* If the request is good and we haven't sent one yet, construct a SearchResultEntry. */
+				res->protocolOp.present = LDAPMessage__protocolOp_PR_searchResEntry;
+				SearchResultEntry_t *searchResEntry = &res->protocolOp.choice.searchResEntry;
+				snprintf(user, sizeof(user), "cn=%s,%s", (const char *)attr->assertionValue.buf,
+					 server->basedn);
+				OCTET_STRING_fromString(&searchResEntry->objectName, user);
+				connection->response_stage = 1;
+			} else {
+				/* Otherwise construct a SearchResultDone. */
+				res->protocolOp.present = LDAPMessage__protocolOp_PR_searchResDone;
+				SearchResultDone_t *searchResDone = &res->protocolOp.choice.searchResDone;
+				if (bad_dn) {
+					asn_long2INTEGER(&searchResDone->resultCode, LDAPResult__resultCode_other);
+					OCTET_STRING_fromString(&searchResDone->diagnosticMessage,
+								"baseobject is invalid");
+				} else if (bad_filter) {
+					asn_long2INTEGER(&searchResDone->resultCode, LDAPResult__resultCode_other);
+					OCTET_STRING_fromString(&searchResDone->diagnosticMessage,
+								"filter not supported");
+				} else {
+					asn_long2INTEGER(&searchResDone->resultCode, LDAPResult__resultCode_success);
+					OCTET_STRING_fromString(&searchResDone->matchedDN, server->basedn);
+				}
+				connection->response_stage = 2;
+			}
+		}
+		status = ldap_connection_send(connection, res);
+	} while (status == RC_OK && connection->response_stage < 2);
+	return status;
 }
 
 int auth_pam(const char *user, const char *pw, char **msg, ev_tstamp *delay)
