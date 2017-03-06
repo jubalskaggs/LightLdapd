2fd458dc418a8d5792e27be33e5d8b142841c524
Donovan Baarda <abo@minkirri.apana.org.au>
2017-03-06 14:10:56 +1100

    Add import guard to pam.h

--- pam.h
+++ pam.h
@@ -3,7 +3,9 @@
  *
  * Licensed under the MIT License. See LICENSE file for details.
  */
-
+#ifndef LIGHTLDAPD_PAM_H
+#define LIGHTLDAPD_PAM_H
 #include <security/pam_appl.h>
 
 int auth_pam(const char *user, const char *pw, char **msg, double *delay);
+#endif  /* LIGHTLDAPD_PAM_H */
